0ec7c4bafb7fe8617c0ac197d6a1c4720ccdf421
code1218 <skjil1218@gmail.com>
2024-02-22 17:53:37 +0900

    feat: emotion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Reset />
+      <SideBar />
+      <Routes>
+
+        <Route path='/mypage' element={<>마이페이지</>}/>
+        <Route path='/board' element={<>게시판</>}/>
+        <Route path='/notice' element={<>공지사항</>}/>
+
+      </Routes>
+    </>
   );
 }
 
