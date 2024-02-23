945d18a020707983474f45809ff86be92fab3cef
code1218 <skjil1218@gmail.com>
2024-02-23 17:50:53 +0900

    feat: Mypage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import { Reset } from 'styled-reset';
 import SideBar from './components/SideBar/SideBar';
+import SideBarTop from './components/SideBarTop/SideBarTop';
+import RootLayout from './components/RootLayout/RootLayout';
+import Mypage from './pages/Mypage/Mypage';
 
 function App() {
   return (
     <>
       <Reset />
       <SideBar />
-      <Routes>
-
-        <Route path='/mypage' element={<>마이페이지</>}/>
-        <Route path='/board' element={<>게시판</>}/>
-        <Route path='/notice' element={<>공지사항</>}/>
-
-      </Routes>
+      <SideBarTop />
+      <RootLayout>
+        <Routes>
+            <Route path='/mypage' element={<Mypage />}/>
+            <Route path='/board' element={<>게시판</>}/>
+            <Route path='/notice' element={<>공지사항</>}/>
+        </Routes>
+      </RootLayout>
+      
     </>
   );
 }
