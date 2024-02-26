62d732e2336a5bd2ba77b2fb57d8b580cfa44178
code1218 <skjil1218@gmail.com>
2024-02-26 12:11:21 +0900

    feat: multiple images

--- src/pages/ImageEx/ImageEx.jsx
+++ src/pages/ImageEx/ImageEx.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useRef, useState } from "react";
 
 const layout = css`
     display: flex;
@@ -9,17 +10,60 @@ const layout = css`
 `;
 
 const imageLayout = css`
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    border: 1px solid #dbdbdb;
+    width: 300px;
+    height: 300px;
+    overflow: hidden;
+    & > img {
+        width: 100%;
+    }
 `;
 
 function ImageEx() {
+    const [ previews, setPreviews ] = useState([]);
+    const imgFileRef = useRef();
+
+    const handleFileChange = (e) => {
+        console.log(e.target.files);
+
+        let promises = [];
+
+        for(let file of e.target.files) {
+            promises = [...promises, new Promise((resolve) => {
+                const fileReader = new FileReader();
+
+                fileReader.onload = (e) => {
+                    console.log(e.target.result);
+                    resolve(e.target.result);
+                }
+    
+                fileReader.readAsDataURL(file);
+            })];
+        }
+
+        Promise.all(promises)
+        .then(result => {
+            console.log(result);
+            setPreviews(result);
+        });
+        
+        
+    }
+
     return (
         <div css={layout}>
-            <div css={imageLayout}>
-                <img src="" alt="" />
-            </div>
-            <input style={{display: "none"}} type="file" />
-            <button>이미지 불러오기</button>
+            {previews.map((preview, index) => 
+                <div key={index} css={imageLayout}>
+                    <img src={preview} alt="" />
+                </div>
+            )}
+            
+            <input style={{display: "none"}} type="file" multiple={true} ref={imgFileRef} onChange={handleFileChange}/>
+            <button onClick={() => imgFileRef.current.click()}>이미지 불러오기</button>
         </div>
     );
 }
