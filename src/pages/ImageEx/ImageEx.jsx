c7c880b40ded9ac556733b4b86337359ca6f67cf
code1218 <skjil1218@gmail.com>
2024-02-26 15:54:14 +0900

    feat: firebase

--- src/pages/ImageEx/ImageEx.jsx
+++ src/pages/ImageEx/ImageEx.jsx
@@ -1,6 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { useRef, useState } from "react";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { storage } from "../../configs/firebase/firebaseConfig";
+import { Line } from "rc-progress";
+// import { app } from "../../configs/firebase/firebaseConfig";
 
 const layout = css`
     display: flex;
@@ -24,46 +28,63 @@ const imageLayout = css`
 `;
 
 function ImageEx() {
+
+    const [ uploadFiles, setUploadFiles ] = useState([]);
     const [ previews, setPreviews ] = useState([]);
+    const [ progressPercent, setProgressPercent ] = useState(0);
     const imgFileRef = useRef();
 
     const handleFileChange = (e) => {
-        console.log(e.target.files);
+        const files = Array.from(e.target.files);
+
+        if(files.length === 0) {
+            imgFileRef.current.value = "";
+            return;
+        }
+
+        setUploadFiles(files);
 
         let promises = [];
 
-        for(let file of e.target.files) {
-            promises = [...promises, new Promise((resolve) => {
-                const fileReader = new FileReader();
+        promises = files.map(file => new Promise((resolve) => {
+            const fileReader = new FileReader();
 
-                fileReader.onload = (e) => {
-                    console.log(e.target.result);
-                    resolve(e.target.result);
-                }
-    
-                fileReader.readAsDataURL(file);
-            })];
-        }
+            fileReader.onload = (e) => {
+                resolve(e.target.result);
+            }
+
+            fileReader.readAsDataURL(file);
+        }));
 
         Promise.all(promises)
         .then(result => {
-            console.log(result);
             setPreviews(result);
-        });
-        
-        
+        });        
+    }
+
+    const handleImageUpload = () => {
+        const file = uploadFiles[0];
+        console.log(uploadFiles);
+        const storageRef = ref(storage, `files/test/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on();
     }
 
     return (
         <div css={layout}>
             {previews.map((preview, index) => 
-                <div key={index} css={imageLayout}>
-                    <img src={preview} alt="" />
-                </div>
+                <>
+                    <div key={index} css={imageLayout}>
+                        <img src={preview} alt="" />
+                    </div>
+                    <Line percent={progressPercent} strokeWidth={4} strokeColor={"#22222"}/>
+                </>
             )}
             
             <input style={{display: "none"}} type="file" multiple={true} ref={imgFileRef} onChange={handleFileChange}/>
             <button onClick={() => imgFileRef.current.click()}>이미지 불러오기</button>
+            <button onClick={handleImageUpload}>이미지 업로드</button>
         </div>
     );
 }
